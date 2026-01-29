1ddf24521cbd4282c50fc0baeecb67af07cb11ee
ankur0704 <sonavaneankur@gmail.com>
2026-01-30 01:09:30 +0530

    project setup + hero section + fileupload(begin)

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Hero } from './components/hero';
+
+function App() {
+
+
+  return <main className='h-screen bg-gray-100  '>
+    <Hero />
+
+  </main>
+
+
+}
+
+export default App;
---
+++ src/components/hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export const Hero = () => {
+    return (
+        <div className='max-w-5xl mx-auto py-40 '>
+            <h1 className='text-6xl font-bold max-w-3xl tracking-tight text-neutral-600'>Create Stunning Thumbnails For Your Videos in Seconds.</h1>
+            <p className='mt-3 text-neutral-600 text-lg'>Generate thumbnails with your image with modern styles and AI</p>
+        </div>
+    )
+}
